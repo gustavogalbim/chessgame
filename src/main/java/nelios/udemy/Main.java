7c6dba1fea3bf69e7f274d4308540bc543718abf
Gustavo Galbim <gustavo.martins@luizalabs.com>
2023-02-26 19:43:04 -0300

    Adicionando peças no tabuleiro

--- src/main/java/nelios/udemy/Main.java
+++ src/main/java/nelios/udemy/Main.java
@@ -3,6 +3,7 @@ package nelios.udemy;
 import nelios.udemy.chess.ChessMatch;
 
 public class Main {
+
     public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
