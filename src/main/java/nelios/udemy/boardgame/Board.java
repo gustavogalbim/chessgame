1ced14b02fe7f932ec575cd2121ecec133e8b31f
Gustavo Galbim <gustavo.martins@luizalabs.com>
2023-03-22 19:07:07 -0300

    Adicionando Exception customizada

--- src/main/java/nelios/udemy/boardgame/Board.java
+++ src/main/java/nelios/udemy/boardgame/Board.java
@@ -2,6 +2,7 @@ package nelios.udemy.boardgame;
 
 import lombok.Getter;
 import lombok.Setter;
+import nelios.udemy.boardgame.exceptions.BoardException;
 
 public class Board {
 
@@ -12,21 +13,50 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns) {
+
+        if(rows < 1 || columns < 1) {
+            throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+        }
+
         this.rows = rows;
         this.columns = columns;
         this.pieces = new Piece[rows][columns];
     }
 
     public Piece piece(int row, int column) {
+        if(!positionExists(row, column)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[row][column];
     }
 
     public Piece piece(Position position) {
+        if(!positionExists(position)) {
+            throw new BoardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColumn()];
     }
 
     public void placePiece(Piece piece, Position position) {
+        if(thereIsPiece(position)) {
+            throw new BoardException("This piece already exists on this position " +position);
+        }
         this.pieces[position.getRow()][position.getColumn()] = piece;
         piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column) {
+        return row>= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+    return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsPiece(Position position) {
+        if(!positionExists(position)) {
+            throw new BoardException("This position already exists");
+        }
+        return piece(position) != null;
     }
 }
---
+++ src/main/java/nelios/udemy/boardgame/exceptions/BoardException.java
@@ -0,0 +1,8 @@
+package nelios.udemy.boardgame.exceptions;
+
+public class BoardException extends RuntimeException {
+
+    public BoardException(String message) {
+        super(message);
+    }
+}
