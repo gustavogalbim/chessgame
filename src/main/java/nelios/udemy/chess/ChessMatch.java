cbd57b8d84006905a5d47dbe35bc12d619108aa4
Gustavo Galbim <gustavo.martins@luizalabs.com>
2023-03-22 19:22:35 -0300

    atualizando comportamento de nova peça

--- src/main/java/nelios/udemy/chess/ChessMatch.java
+++ src/main/java/nelios/udemy/chess/ChessMatch.java
@@ -1,7 +1,6 @@
 package nelios.udemy.chess;
 
 import nelios.udemy.boardgame.Board;
-import nelios.udemy.boardgame.Position;
 import nelios.udemy.chess.pieces.King;
 import nelios.udemy.chess.pieces.Rook;
 
@@ -26,10 +25,14 @@ public class ChessMatch {
         return matriz;
     }
 
+    private void placeNewPiece(char column, int row, ChessPiece piece) {
+        board.placePiece(piece, new ChessPosition(column, row).toPosition());
+    }
+
     private void initialSetup() {
-        board.placePiece(new Rook(board, Color.WHITE), new Position(2, 1));
-        board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
-        board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
+        placeNewPiece('b', 6, new Rook(board, Color.WHITE));
+        placeNewPiece('e', 8, new King(board, Color.BLACK));
+        placeNewPiece('e', 1, new King(board, Color.BLACK));
     }
 
 }
